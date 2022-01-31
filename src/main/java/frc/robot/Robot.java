7112db70d5c39a7da3d36103138bc176f352515b
Briojas <blake.a.riojas@gmail.com>
2022-01-31 11:30:41 -0600

    .

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -34,8 +34,8 @@ public class Robot extends TimedRobot {
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
     
-    comp = new Compressor(PneumaticsModuleType.REVPH);
-    // comp = new Compressor(1, PneumaticsModuleType.REVPH); //try this if the top one doesnt work. If this doesn't work, set the '1' to '0' and try again. 
+   // comp = new Compressor(PneumaticsModuleType.REVPH);
+    comp = new Compressor(1, PneumaticsModuleType.REVPH); //try this if the top one doesnt work. If this doesn't work, set the '1' to '0' and try again. 
     comp.enableAnalog(80, 120);
 
           //not sure when or how we would use these yet. 
