6189d633825c53ee2541420775220154b275b0f9
Briojas <blake.a.riojas@gmail.com>
2022-02-28 20:20:30 -0600

    Change cap percentage to 75%

--- src/main/java/frc/robot/subsystems/WheelOfFortune.java
+++ src/main/java/frc/robot/subsystems/WheelOfFortune.java
@@ -49,8 +49,8 @@ public class WheelOfFortune extends SubsystemBase{
     */
     public void spinWheel(double speed)
     {
-        leftWheel.set(-speed*0.60);
-        rightWheel.set(speed*0.60);
+        leftWheel.set(-speed*0.75);
+        rightWheel.set(speed*0.75);
     }
 
 
